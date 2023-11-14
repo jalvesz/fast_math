fda8fd013f89f28fd02799a989015071d733fed8
Jose Alves <jose.alves@transvalor.com>
2023-11-14 23:20:22 +0100

    kahan sum and kahan dot_product

--- src/fast_dotp.f90
+++ src/fast_dotp.f90
@@ -7,10 +7,11 @@ module fast_dotp
     !! A faster and more accurate implementation of the dot_product intrinsic. 
     !! It uses the same principle as fsum_chunk but considering local multiplications that can be vectorized for faster summation.
     use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
+    use fast_utilities
     implicit none
     private
     
-    public :: fprod
+    public :: fprod, fprod_kahan
     integer, parameter :: chunk64 = 32
     integer, parameter :: chunk32 = 64
     
@@ -19,6 +20,13 @@ module fast_dotp
         module procedure fprod_sp_weighted
         module procedure fprod_dp
         module procedure fprod_dp_weighted
+    end interface
+
+    interface fprod_kahan
+        module procedure fprod_kahan_sp
+        module procedure fprod_kahan_sp_weighted
+        module procedure fprod_kahan_dp
+        module procedure fprod_kahan_dp_weighted
     end interface
     
     contains
@@ -128,4 +136,121 @@ module fast_dotp
           p = p + abatch(i)+abatch(chunk/2+i)
         end do
     end function
+
+    pure function fprod_kahan_sp(a,b) result(p)
+        integer, parameter :: wp = sp
+        integer, parameter :: chunk = chunk32
+        real(wp), intent(in) :: a(:)
+        real(wp), intent(in) :: b(:)
+        real(wp) :: p
+        ! --
+        real(wp) :: sbatch(chunk)
+        real(wp) :: cbatch(chunk)
+        integer :: i, n, dr, rr
+        ! -----------------------------
+        n  = size(a)
+        dr = n/chunk
+        rr = n - dr*chunk
+        
+        sbatch(:) = 0.0_wp
+        cbatch(:) = 0.0_wp
+        do concurrent( i = 1:dr )
+          call vkahans( a(chunk*i-chunk+1:chunk*i) * b(chunk*i-chunk+1:chunk*i) , sbatch(1:chunk) , cbatch(1:chunk) )
+        end do
+        call vkahans( a(size(a)-rr+1:size(a)) * b(size(a)-rr+1:size(a)), sbatch(1:rr), cbatch(1:rr) ) 
+        
+        p = 0.0_wp
+        do i = 1,chunk
+          call vkahans( sbatch(i) , p , cbatch(i) )
+        end do
+    end function
+
+    pure function fprod_kahan_dp(a,b) result(p)
+        integer, parameter :: wp = dp
+        integer, parameter :: chunk = chunk64
+        real(wp), intent(in) :: a(:)
+        real(wp), intent(in) :: b(:)
+        real(wp) :: p
+        ! --
+        real(wp) :: sbatch(chunk)
+        real(wp) :: cbatch(chunk)
+        integer :: i, n, dr, rr
+        ! -----------------------------
+        n  = size(a)
+        dr = n/chunk
+        rr = n - dr*chunk
+        
+        sbatch(:) = 0.0_wp
+        cbatch(:) = 0.0_wp
+        do concurrent( i = 1:dr )
+          call vkahans( a(chunk*i-chunk+1:chunk*i) * b(chunk*i-chunk+1:chunk*i) , sbatch(1:chunk) , cbatch(1:chunk) )
+        end do
+        call vkahans( a(size(a)-rr+1:size(a)) * b(size(a)-rr+1:size(a)), sbatch(1:rr), cbatch(1:rr) ) 
+        
+        p = 0.0_wp
+        do i = 1,chunk
+          call vkahans( sbatch(i) , p , cbatch(i) )
+        end do
+    end function
+
+    pure function fprod_kahan_sp_weighted(a,b,w) result(p)
+        integer, parameter :: wp = sp
+        integer, parameter :: chunk = chunk32
+        real(wp), intent(in) :: a(:)
+        real(wp), intent(in) :: b(:)
+        real(wp), intent(in) :: w(:)
+        real(wp) :: p
+        ! --
+        real(wp) :: sbatch(chunk)
+        real(wp) :: cbatch(chunk)
+        integer :: i, n, dr, rr
+        ! -----------------------------
+        n  = size(a)
+        dr = n/chunk
+        rr = n - dr*chunk
+        
+        sbatch(:) = 0.0_wp
+        cbatch(:) = 0.0_wp
+        do concurrent( i = 1:dr )
+          call vkahans( a(chunk*i-chunk+1:chunk*i) * b(chunk*i-chunk+1:chunk*i) * &
+                w(chunk*i-chunk+1:chunk*i) , sbatch(1:chunk) , cbatch(1:chunk) )
+        end do
+        call vkahans( a(size(a)-rr+1:size(a)) * b(size(a)-rr+1:size(a)) * w(size(a)-rr+1:size(a)), sbatch(1:rr), cbatch(1:rr) ) 
+        
+        p = 0.0_wp
+        do i = 1,chunk
+          call vkahans( sbatch(i) , p , cbatch(i) )
+        end do
+    end function
+
+    pure function fprod_kahan_dp_weighted(a,b,w) result(p)
+        integer, parameter :: wp = dp
+        integer, parameter :: chunk = chunk64
+        real(wp), intent(in) :: a(:)
+        real(wp), intent(in) :: b(:)
+        real(wp), intent(in) :: w(:)
+        real(wp) :: p
+        ! --
+        real(wp) :: sbatch(chunk)
+        real(wp) :: cbatch(chunk)
+        integer :: i, n, dr, rr
+        ! -----------------------------
+        n  = size(a)
+        dr = n/chunk
+        rr = n - dr*chunk
+        
+        sbatch(:) = 0.0_wp
+        cbatch(:) = 0.0_wp
+        do concurrent( i = 1:dr )
+          call vkahans( a(chunk*i-chunk+1:chunk*i) * b(chunk*i-chunk+1:chunk*i) * &
+                w(chunk*i-chunk+1:chunk*i) , sbatch(1:chunk) , cbatch(1:chunk) )
+        end do
+        call vkahans( a(size(a)-rr+1:size(a)) * b(size(a)-rr+1:size(a)) * w(size(a)-rr+1:size(a)), sbatch(1:rr), cbatch(1:rr) ) 
+        
+        p = 0.0_wp
+        do i = 1,chunk
+          call vkahans( sbatch(i) , p , cbatch(i) )
+        end do
+    end function
+
   end module fast_dotp
--- src/fast_sum.f90
+++ src/fast_sum.f90
@@ -8,10 +8,11 @@ module fast_sum
   !! By default, "fsum" will use the fsum_chunk approach. This method is at worst, one order of magnitud more accurate that "sum" and between 1.5 to 10 times faster
   !! A second approach is also proposed, "fsum_pair" which is the most accurate approach. cpu time can vary between x2 times slower or sometimes faster than intrinsic sum.
   use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
+  use fast_utilities
   implicit none
   private
 
-  public :: fsum, fsum_pair
+  public :: fsum, fsum_kahan
   integer, parameter :: chunk64 = 32
   integer, parameter :: chunk32 = 64
   
@@ -23,90 +24,15 @@ module fast_sum
       module procedure fsum_chunk_1d_dp_mask
   end interface
 
-  interface fsum_pair
-    !! Source: https://fortran-lang.discourse.group/t/some-intrinsic-sums/5760/26
-      module procedure fsum_pair_1d_sp
-      module procedure fsum_pair_1d_sp_mask
-      module procedure fsum_pair_1d_dp
-      module procedure fsum_pair_1d_dp_mask
+  interface fsum_kahan
+      module procedure fsum_kahan_1d_sp
+      module procedure fsum_kahan_1d_sp_mask
+      module procedure fsum_kahan_1d_dp
+      module procedure fsum_kahan_1d_dp_mask
   end interface
-  
+
   contains
 
-  pure recursive function fsum_pair_1d_sp(a) result(bout)
-      integer, parameter :: wp=sp
-      integer, parameter :: chunk = chunk32
-      real(wp), intent(in) :: a(:)
-      real(wp) :: bout
-      integer :: n, i
-      ! -----------------------------
-      n = size(a)
-      if (n.le.chunk) then
-        bout = 0.0_wp
-        do concurrent(i = 1:n)
-           bout = bout + a(i)
-        end do
-      else
-         bout = fsum_pair(a(1:n/2)) + fsum_pair(a(n/2+1:n))
-      end if
-  end function
-
-  pure recursive function fsum_pair_1d_dp(a) result(bout)
-      integer, parameter :: wp=dp
-      integer, parameter :: chunk = chunk64
-      real(wp), intent(in) :: a(:)
-      real(wp) :: bout
-      integer :: n, i
-      ! -----------------------------
-      n = size(a)
-      if (n.le.chunk) then
-        bout = 0.0_wp
-        do concurrent(i = 1:n)
-           bout = bout + a(i)
-        end do
-      else
-         bout = fsum_pair(a(1:n/2)) + fsum_pair(a(n/2+1:n))
-      end if
-  end function
-  
-  pure recursive function fsum_pair_1d_sp_mask(a,mask) result(bout)
-      integer, parameter :: wp=sp
-      integer, parameter :: chunk = chunk32
-      real(wp), intent(in) :: a(:)
-      logical, intent(in) :: mask(:)
-      real(wp) :: bout
-      integer :: n, i
-      ! -----------------------------
-      n = size(a)
-      if (n.le.chunk) then
-         bout = 0.0_wp
-         do concurrent(i = 1:n)
-            if(mask(i)) bout = bout + a(i)
-         end do
-      else
-         bout = fsum_pair(a(1:n/2),mask(1:n/2)) + fsum_pair(a(n/2+1:n),mask(n/2+1:n))
-      end if
-  end function
-
-  pure recursive function fsum_pair_1d_dp_mask(a,mask) result(bout)
-      integer, parameter :: wp=dp
-      integer, parameter :: chunk = chunk64
-      real(wp), intent(in) :: a(:)
-      logical, intent(in) :: mask(:)
-      real(wp) :: bout
-      integer :: n, i
-      ! -----------------------------
-      n = size(a)
-      if (n.le.chunk) then
-         bout = 0.0_wp
-         do concurrent(i = 1:n)
-            if(mask(i)) bout = bout + a(i)
-         end do
-      else
-         bout = fsum_pair(a(1:n/2),mask(1:n/2)) + fsum_pair(a(n/2+1:n),mask(n/2+1:n))
-      end if
-  end function  
-  
   pure function fsum_chunk_1d_sp(a) result(sout)
       integer, parameter :: wp = sp
       integer, parameter :: chunk = chunk32
@@ -204,4 +130,111 @@ module fast_sum
         sout = sout + abatch(i)+abatch(chunk/2+i)
       end do
   end function
+
+  pure function fsum_kahan_1d_sp(a) result(sout)
+      integer, parameter :: wp = sp
+      integer, parameter :: chunk = chunk32
+      real(wp), intent(in) :: a(:)
+      real(wp) :: sout
+      ! --
+      real(wp) :: sbatch(chunk)
+      real(wp) :: cbatch(chunk)
+      integer :: i, dr, rr
+      ! -----------------------------
+      dr = size(a)/(chunk)
+      rr = size(a) - dr*chunk     
+      sbatch(:) = 0.0_wp
+      cbatch(:) = 0.0_wp
+      do concurrent( i = 1:dr )
+        call vkahans( a(chunk*i-chunk+1:chunk*i) , sbatch(1:chunk) , cbatch(1:chunk) )
+      end do
+      call vkahans( a(size(a)-rr+1:size(a)) , sbatch(1:rr) , cbatch(1:rr) )      
+      
+      sout = 0.0_wp
+      do i = 1,chunk
+        call vkahans( sbatch(i) , sout , cbatch(i) )
+      end do
+  end function
+
+  pure function fsum_kahan_1d_dp(a) result(sout)
+      integer, parameter :: wp = dp
+      integer, parameter :: chunk = chunk64
+      real(wp), intent(in) :: a(:)
+      real(wp) :: sout
+      ! --
+      real(wp) :: sbatch(chunk)
+      real(wp) :: cbatch(chunk)
+      integer :: i, dr, rr
+      ! -----------------------------
+      dr = size(a)/(chunk)
+      rr = size(a) - dr*chunk     
+      sbatch(:) = 0.0_wp
+      cbatch(:) = 0.0_wp
+      do concurrent( i = 1:dr )
+        call vkahans( a(chunk*i-chunk+1:chunk*i) , sbatch(1:chunk) , cbatch(1:chunk) )
+      end do
+      call vkahans( a(size(a)-rr+1:size(a)) , sbatch(1:rr) , cbatch(1:rr) )      
+      
+      sout = 0.0_wp
+      do i = 1,chunk
+        call vkahans( sbatch(i) , sout , cbatch(i) )
+      end do
+  end function
+
+  pure function fsum_kahan_1d_sp_mask(a,mask) result(sout)
+      integer, parameter :: wp = sp
+      integer, parameter :: chunk = chunk32
+      real(wp), intent(in) :: a(:)
+      logical, intent(in) :: mask(:)
+      real(wp) :: sout
+      ! --
+      real(wp) :: sbatch(chunk)
+      real(wp) :: cbatch(chunk)
+      integer :: i, j, dr, rr
+      ! -----------------------------
+      dr = size(a)/(chunk)
+      rr = size(a) - dr*chunk     
+      sbatch(:) = 0.0_wp
+      cbatch(:) = 0.0_wp
+      do concurrent( i = 1:dr , j=1:chunk )
+        if(mask(chunk*i-chunk+j)) call vkahans( a(chunk*i-chunk+j), sbatch(j) , cbatch(j) )
+      end do
+      do concurrent( i = 1:rr )
+        if(mask(size(a)-rr+i)) call vkahans( a(size(a)-rr+i), sbatch(i) , cbatch(i) )
+      end do
+
+      sout = 0.0_wp
+      do i = 1,chunk
+        call vkahans( sbatch(i) , sout , cbatch(i) )
+      end do
+  end function
+
+  pure function fsum_kahan_1d_dp_mask(a,mask) result(sout)
+      integer, parameter :: wp = dp
+      integer, parameter :: chunk = chunk64
+      real(wp), intent(in) :: a(:)
+      logical, intent(in) :: mask(:)
+      real(wp) :: sout
+      ! --
+      real(wp) :: sbatch(chunk)
+      real(wp) :: cbatch(chunk)
+      integer :: i, j, dr, rr
+      ! -----------------------------
+      dr = size(a)/(chunk)
+      rr = size(a) - dr*chunk     
+      sbatch(:) = 0.0_wp
+      cbatch(:) = 0.0_wp
+      do concurrent( i = 1:dr , j=1:chunk )
+        if(mask(chunk*i-chunk+j)) call vkahans( a(chunk*i-chunk+j), sbatch(j) , cbatch(j) )
+      end do
+      do concurrent( i = 1:rr )
+        if(mask(size(a)-rr+i)) call vkahans( a(size(a)-rr+i), sbatch(i) , cbatch(i) )
+      end do
+
+      sout = 0.0_wp
+      do i = 1,chunk
+        call vkahans( sbatch(i) , sout , cbatch(i) )
+      end do
+  end function
+
 end module fast_sum
